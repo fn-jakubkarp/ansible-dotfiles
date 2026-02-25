940a1b8710e1495a0b1116ae23ac0980991a690c
lijin <lirinlabs@gmail.com>
2026-02-25 18:31:27 +0100

    motidy setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
+# #!/bin/bash
+
+# set -e
+
+# echo "Checking if Ansible is installed..."
+# if ! command -v ansible &> /dev/null; then
+#     echo "Ansible not found. Installing..."
+#     sudo apt update
+#     sudo apt install -y ansible
+# else
+#     echo "Ansible is already installed."
+# fi
+
+# echo "Running playbook with networking tag..."
+# ansible-playbook main.yml --tags networking -K
+
+# echo "Done!"
+
 #!/bin/bash
 
 set -e
@@ -5,13 +23,19 @@ set -e
 echo "Checking if Ansible is installed..."
 if ! command -v ansible &> /dev/null; then
     echo "Ansible not found. Installing..."
-    sudo apt update
-    sudo apt install -y ansible
+    
+    apt update
+
+    apt install -y gnupg2 software-properties-common
+    
+    add-apt-repository --yes --update ppa:ansible/ansible
+    
+    apt install -y ansible
 else
     echo "Ansible is already installed."
 fi
 
-echo "Running playbook with networking tag..."
-ansible-playbook main.yml --tags networking -K
+echo "Running playbook..."
+ansible-playbook main.yml
 
 echo "Done!"
