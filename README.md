2d5b3abaca163c72a9380d38997a7774a57d529e
lijin <lirinlabs@gmail.com>
2026-02-26 11:38:47 +0100

    fix legacy code and sudo setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Important!
+
+Do not run as root or with sudo, as the playbook is designed to be run as a regular user. Running `sudo ./setup.sh` will put the dotfiles in `/root` instead of `/home/yourusername` directory.
+
+---
+
+
 If used through WSL, this dotfiles setup includes configuration for SSH agent socket forwarding between Windows and WSL using `socat` and `npiperelay`.
 The `socat` package is installed in the common tasks, and the `.zshrc`.
 Setup requires `npiperelay.exe` to be available in the system PATH on Windows.
--- setup.sh
+++ setup.sh
@@ -8,12 +8,11 @@ echo "Checking if Ansible is installed..."
 if ! command -v ansible &> /dev/null; then
     echo "Ansible not found. Installing..."
     
-    apt update
-    apt install -y tzdata
-    
-    apt install -y gnupg2 software-properties-common
-    add-apt-repository --yes --update ppa:ansible/ansible
-    apt install -y ansible
+    sudo apt update
+    sudo apt install -y tzdata
+    sudo apt install -y gnupg2 software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install -y ansible
 else
     echo "Ansible is already installed."
 fi
